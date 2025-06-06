c769df56fcabec3150be2e9b66d55d165cd2dd16
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-06-06 17:51:58 +0530

    feat: implement Visitor pattern for handling invoices and shipping costs for products

--- 04_BehaviouralDesignPatterns/07_VisitorPattern/Main.java
+++ 04_BehaviouralDesignPatterns/07_VisitorPattern/Main.java
@@ -1,3 +1,115 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface Item {
+    void accept(ItemVisitor visitor);
+}
+
+interface ItemVisitor {
+    void visit(PhysicalProduct physicalProduct);
+
+    void visit(DigitalProduct digitalProduct);
+
+    void visit(GiftCard giftCard);
+}
+
+class PhysicalProduct implements Item {
+    String name;
+    double weight;
+
+    public PhysicalProduct(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public void accept(ItemVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class DigitalProduct implements Item {
+    String name;
+    double size;
+
+    public DigitalProduct(String name, double size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    @Override
+    public void accept(ItemVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class GiftCard implements Item {
+    String code;
+    double value;
+
+    public GiftCard(String code, double value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    @Override
+    public void accept(ItemVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class InvoiceVisitor implements ItemVisitor {
+    @Override
+    public void visit(PhysicalProduct physicalProduct) {
+        System.out.println("Printing invoice for physical product: " + physicalProduct.name);
+    }
+
+    @Override
+    public void visit(DigitalProduct digitalProduct) {
+        System.out.println("Printing invoice for digital product: " + digitalProduct.name);
+    }
+
+    @Override
+    public void visit(GiftCard giftCard) {
+        System.out.println("Printing invoice for gift card: " + giftCard.code);
+    }
+}
+
+class ShippingCostVisitor implements ItemVisitor {
+    @Override
+    public void visit(PhysicalProduct physicalProduct) {
+        System.out.println("Shipping cost for physical product: $" + (physicalProduct.weight * 0.5));
+    }
+
+    @Override
+    public void visit(DigitalProduct digitalProduct) {
+        System.out.println("No shipping cost for digital product: " + digitalProduct.name);
+    }
+
+    @Override
+    public void visit(GiftCard giftCard) {
+        System.out.println("No shipping cost for gift card: " + giftCard.code);
+    }
+}
+
 public class Main {
-    
+    public static void main(String[] args) {
+        List<Item> items = new ArrayList<>();
+        Item physicalProduct = new PhysicalProduct("Laptop", 2.5);
+        Item digitalProduct = new DigitalProduct("E-book", 0.1);
+        Item giftCard = new GiftCard("GC12345", 50.0);
+
+        items.add(physicalProduct);
+        items.add(digitalProduct);
+        items.add(giftCard);
+
+        ItemVisitor invoiceVisitor = new InvoiceVisitor();
+        ItemVisitor shippingCostVisitor = new ShippingCostVisitor();
+
+        for (Item item : items) {
+            item.accept(invoiceVisitor);
+            item.accept(shippingCostVisitor);
+        }
+    }
 }
--- 04_BehaviouralDesignPatterns/07_VisitorPattern/badCode.java
+++ 04_BehaviouralDesignPatterns/07_VisitorPattern/badCode.java
@@ -1,5 +1,61 @@
-package 04_BehaviouralDesignPatterns.01_IteratorPattern;
+class PhysicalProduct {
+    void printInvoice() {
+        System.out.println("Printing invoice for physical product.");
+    }
+
+    double calculateShippingCost() {
+        return 5.99; // Flat shipping cost for physical products
+    }
+}
+
+class DigitalProduct {
+    void printInvoice() {
+        System.out.println("Printing invoice for digital product.");
+    }
+    // no shipping cost for digital products
+}
+
+class GiftCard {
+    void printInvoice() {
+        System.out.println("Printing invoice for gift card.");
+    }
+
+    double calculateDiscount() {
+        return 10.00; // Flat discount for gift cards
+    }
+}
 
 public class badCode {
-    
+    public static void main(String[] args) {
+        PhysicalProduct physicalProduct = new PhysicalProduct();
+        DigitalProduct digitalProduct = new DigitalProduct();
+        GiftCard giftCard = new GiftCard();
+
+        // Printing invoices
+        physicalProduct.printInvoice();
+        digitalProduct.printInvoice();
+        giftCard.printInvoice();
+
+        // Calculating shipping cost for physical product
+        double shippingCost = physicalProduct.calculateShippingCost();
+        System.out.println("Shipping cost for physical product: $" + shippingCost);
+
+        // Calculating discount for gift card
+        double discount = giftCard.calculateDiscount();
+        System.out.println("Discount for gift card: $" + discount);
+
+        for (Item item : cart) {
+            if (item instanceof PhysicalProduct) {
+                ((PhysicalProduct) item).printInvoice();
+                double shippingCost = ((PhysicalProduct) item).calculateShippingCost();
+                System.out.println("Shipping cost for physical product: $" + shippingCost);
+            } else if (item instanceof DigitalProduct) {
+                ((DigitalProduct) item).printInvoice();
+            } else if (item instanceof GiftCard) {
+                ((GiftCard) item).printInvoice();
+                double discount = ((GiftCard) item).calculateDiscount();
+                System.out.println("Discount for gift card: $" + discount);
+            }
+        }
+    }
 }
