c8e96489a4790518610e58c6277d9f763402806a
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-06-06 16:27:35 +0530

    feat: implement Command pattern with Light, AC, and Fan controls in NaiveRemoteControl

--- 04_BehaviouralDesignPatterns/04_CommandPattern/Main.java
+++ 04_BehaviouralDesignPatterns/04_CommandPattern/Main.java
@@ -1,3 +1,196 @@
+
+import java.util.Stack;
+
+class Light {
+    public void turnOn() {
+        System.out.println("Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
+
+class AC {
+    public void turnOn() {
+        System.out.println("AC is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("AC is OFF");
+    }
+}
+
+class Fan {
+    public void turnOn() {
+        System.out.println("Fan is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Fan is OFF");
+    }
+}
+
+interface Command {
+    void execute();
+
+    void undo();
+}
+
+class TurnOnLightCommand implements Command {
+    private Light light;
+
+    public TurnOnLightCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        light.turnOff();
+    }
+}
+
+class TurnOnACCommand implements Command {
+    private AC ac;
+
+    public TurnOnACCommand(AC ac) {
+        this.ac = ac;
+    }
+
+    @Override
+    public void execute() {
+        ac.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        ac.turnOff();
+    }
+}
+
+class TurnOnFanCommand implements Command {
+    private Fan fan;
+
+    public TurnOnFanCommand(Fan fan) {
+        this.fan = fan;
+    }
+
+    @Override
+    public void execute() {
+        fan.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        fan.turnOff();
+    }
+}
+
+class TurnOffLightCommand implements Command {
+    private Light light;
+
+    public TurnOffLightCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+
+    @Override
+    public void undo() {
+        light.turnOn();
+    }
+}
+
+class TurnOffACCommand implements Command {
+    private AC ac;
+
+    public TurnOffACCommand(AC ac) {
+        this.ac = ac;
+    }
+
+    @Override
+    public void execute() {
+        ac.turnOff();
+    }
+
+    @Override
+    public void undo() {
+        ac.turnOn();
+    }
+}
+
+class TurnOffFanCommand implements Command {
+    private Fan fan;
+
+    public TurnOffFanCommand(Fan fan) {
+        this.fan = fan;
+    }
+
+    @Override
+    public void execute() {
+        fan.turnOff();
+    }
+
+    @Override
+    public void undo() {
+        fan.turnOn();
+    }
+}
+
+// Invoker
+// 6 command
+class RemoteControl {
+    private Command[] buttons = new Command[6];
+    private Stack<Command> commandHistory = new Stack<>();
+
+    public void setCommand(int slot, Command command) {
+        buttons[slot] = command;
+    }
+
+    public void pressButton(int slot) {
+        if (buttons[slot] != null) {
+            buttons[slot].execute();
+            commandHistory.push(buttons[slot]);
+        }
+    }
+
+    public void pressUndo() {
+        if (!commandHistory.isEmpty()) {
+            Command lastCommand = commandHistory.pop();
+            lastCommand.undo();
+        }
+    }
+}
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Light light = new Light();
+        AC ac = new AC();
+        Fan fan = new Fan();
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.setCommand(0, new TurnOnLightCommand(light));
+        remote.setCommand(1, new TurnOffLightCommand(light));
+        remote.setCommand(2, new TurnOnACCommand(ac));
+        remote.setCommand(3, new TurnOffACCommand(ac));
+        remote.setCommand(4, new TurnOnFanCommand(fan));
+        remote.setCommand(5, new TurnOffFanCommand(fan));
+
+        remote.pressButton(0); // Light ON
+        remote.pressButton(2); // AC ON
+        remote.pressButton(4); // Fan ON
+
+        remote.pressUndo(); // Fan OFF
+        remote.pressUndo(); // AC OFF
+        remote.pressUndo(); // Light OFF
+    }
 }
--- 04_BehaviouralDesignPatterns/04_CommandPattern/badCode.java
+++ 04_BehaviouralDesignPatterns/04_CommandPattern/badCode.java
@@ -1,5 +1,120 @@
-package 04_BehaviouralDesignPatterns.01_IteratorPattern;
+class Light {
+    public void turnOn() {
+        System.out.println("Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
+
+class AC {
+    public void turnOn() {
+        System.out.println("AC is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("AC is OFF");
+    }
+}
+
+class Fan {
+    public void turnOn() {
+        System.out.println("Fan is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Fan is OFF");
+    }
+}
+
+class NaiveRemoteControl {
+    private Light light;
+    private AC ac;
+    private Fan fan;
+    private String lastAction = "";
+
+    public NaiveRemoteControl(Light light, AC ac, Fan fan) {
+        this.light = light;
+        this.ac = ac;
+        this.fan = fan;
+    }
+
+    public void turnOnLight() {
+        light.turnOn();
+        lastAction = "Light ON";
+    }
+
+    public void turnOffLight() {
+        light.turnOff();
+        lastAction = "Light OFF";
+    }
+
+    public void turnOnAC() {
+        ac.turnOn();
+        lastAction = "AC ON";
+    }
+
+    public void turnOffAC() {
+        ac.turnOff();
+        lastAction = "AC OFF";
+    }
+
+    public void turnOnFan() {
+        fan.turnOn();
+        lastAction = "Fan ON";
+    }
+
+    public void turnOffFan() {
+        fan.turnOff();
+        lastAction = "Fan OFF";
+    }
+
+    public void pressUndo() {
+        switch (lastAction) {
+            case "Light ON":
+                light.turnOff();
+                lastAction = "Light OFF";
+                break;
+            case "Light OFF":
+                light.turnOn();
+                lastAction = "Light ON";
+                break;
+            case "AC ON":
+                ac.turnOff();
+                lastAction = "AC OFF";
+                break;
+            case "AC OFF":
+                ac.turnOn();
+                lastAction = "AC ON";
+                break;
+            case "Fan ON":
+                fan.turnOff();
+                lastAction = "Fan OFF";
+                break;
+            case "Fan OFF":
+                fan.turnOn();
+                lastAction = "Fan ON";
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+}
 
 public class badCode {
-    
+    public static void main(String[] args) {
+        Light light = new Light();
+        AC ac = new AC();
+        Fan fan = new Fan();
+        NaiveRemoteControl remote = new NaiveRemoteControl(light, ac, fan);
+
+        remote.turnOnLight();
+        remote.turnOnAC();
+        remote.turnOnFan();
+
+        remote.pressUndo(); // Should turn off the fan
+        remote.pressUndo(); // Should turn off the AC
+        remote.pressUndo(); // Should turn off the light
+    }
 }
